c25213492a7e9eb088e12343dec28d831494066c
Jiang XiaoJun <jiangjun9166@foxmail.com>
2018-06-27 20:50:46 +0800

    add new code 20:50

--- a.js
+++ a.js
@@ -2,3 +2,4 @@ console.log("2018/6/27 17:00 新增js文件")
 console.log("16:00 远程仓库修改代码")
 
 console.log("19:40 本地从远程clone,只有master分支，然后 git checkout -b dev origin/dev 创建本地dev分支 并修改")
+console.log("20:50 再次从本地新增代码")

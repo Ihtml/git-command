5ba1b73116451aff00ab3524dc9e09727a2e6176
Jiang XiaoJun <jiangjun9166@foxmail.com>
2018-06-27 21:01:58 +0800

    change origin from https to ssh

--- a.js
+++ a.js
@@ -3,3 +3,5 @@ console.log("16:00 远程仓库修改代码")
 
 console.log("19:40 本地从远程clone,只有master分支，然后 git checkout -b dev origin/dev 创建本地dev分支 并修改")
 console.log("20:50 再次从本地新增代码")
+
+console.log("20:58 解决每次push都要输入用户名和密码的问题，因为clone的时候使用的是HTTPS的方式，执行git remote rm origin，git remote add origin git@github.com:Kaixinzuizhongyao/git-test1.git即可")

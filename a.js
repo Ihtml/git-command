f510a74769fc39adb2c0aa65272b2921f145ea5f
Jiang XiaoJun <jiangjun9166@foxmail.com>
2018-06-27 21:43:18 +0800

    setted SSH Key

--- a.js
+++ a.js
@@ -5,3 +5,4 @@ console.log("19:40 本地从远程clone,只有master分支，然后 git checkout
 console.log("20:50 再次从本地新增代码")
 
 console.log("20:58 解决每次push都要输入用户名和密码的问题，因为clone的时候使用的是HTTPS的方式，执行git remote rm origin，git remote add origin git@github.com:Kaixinzuizhongyao/git-test1.git即可")
+console.log("21:39 重新设置SSH Key,添加公开密钥")

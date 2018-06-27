eacf545098f069a0054785bde57d667e4c4201a5
Jiang XiaoJun <jiangjun9166@foxmail.com>
2018-06-27 22:14:59 +0800

    remove feature-D branch

--- a.js
+++ a.js
@@ -6,3 +6,4 @@ console.log("20:50 再次从本地新增代码")
 
 console.log("20:58 解决每次push都要输入用户名和密码的问题，因为clone的时候使用的是HTTPS的方式，执行git remote rm origin，git remote add origin git@github.com:Kaixinzuizhongyao/git-test1.git即可")
 console.log("21:39 重新设置SSH Key,添加公开密钥")
+console.log("22:12 删除远程分支feature-D 命令：git push origin :feature-D")
